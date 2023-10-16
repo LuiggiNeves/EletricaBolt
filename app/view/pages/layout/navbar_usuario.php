0463e33cd4ef358eafeb9833f5740313128a5000
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-10-16 14:05:01 -0300

    feat: painel do usuário

---
+++ app/view/pages/layout/navbar_usuario.php
@@ -0,0 +1,51 @@
+<div class="container-fluid mt-3">
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-10 rounded bg-white border shadow-sm">
+            <div class="row p-4">
+                <a href="../painel/" class="divPainel text-decoration-none col-sm-3 col-md-2 mx-1 text-center py-1 px-5 my-1 rounded btn-selecionar-item-navbar">
+                    <span>
+                        <div>
+                            <i class="bi bi-speedometer2"></i>
+                        </div>
+
+                        <div>
+                            Painel
+                        </div>
+                    </span>
+                </a>
+                <a href="../categorias/" class="divCategorias text-decoration-none col-sm-3 col-md-2 mx-1 text-center py-1 px-5 my-1 rounded btn-selecionar-item-navbar">
+                    <span>
+                        <div>
+                            <i class="bi bi-grid"></i>
+                        </div>
+
+                        <div>
+                            Categorias
+                        </div>
+                    </span>
+                </a>
+                <a href="../produtos/" class="divProdutos text-decoration-none col-sm-3 col-md-2 mx-1 text-center py-1 px-5 my-1 rounded btn-selecionar-item-navbar">
+                    <span>
+                        <div>
+                            <i class="bi bi-box"></i>
+                        </div>
+
+                        <div>
+                            Produtos
+                        </div>
+                    </span>
+                </a>
+                <a href="../usuarios/" class="divUsuarios text-decoration-none col-sm-3 col-md-2 mx-1 text-center py-1 px-5 my-1 rounded btn-selecionar-item-navbar">
+                    <span>
+                        <div>
+                            <i class="bi bi-people"></i>
+                        </div>
+                        <div>
+                            Usuários
+                        </div>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ app/view/pages/paineis/usuario/usuarios.php
@@ -0,0 +1,47 @@
+<?php
+include('../../layout/head.php');
+?>
+
+<?php
+include('../../layout/auth.php');
+?>
+
+</head>
+
+<body class="bg-light">
+
+    <?php
+    include('../../layout/navbar_usuario.php');
+    ?>
+
+    <div class="container-fluid mt-3">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-10 my-1">
+                <span class="text-muted">
+                    <i class="bi bi-person-fill"></i>
+                    <?= unserialize($_SESSION["usuario_logado"])->getNome(); ?>
+                </span>
+            </div>
+
+            <div class="col-sm-12 col-md-10 rounded bg-white border shadow-sm" style="height: 400px;">
+                <div class="row p-4">
+                    <div class="col-sm-12 col-md-12">
+                        <h3>
+                            <i class="bi bi-people"></i>
+                            Usuários
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include('../../layout/footer.php');
+    ?>
+
+    <script>
+        $(".divUsuarios").addClass("btn-item-navbar-selecionado");
+    </script>
+
+</body>
