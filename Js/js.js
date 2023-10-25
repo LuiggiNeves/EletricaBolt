1c4fd8ed140a43e2996e0dead7640c76866f6487
Luiggi Neves <luiggineves21@gmail.com>
2023-10-24 23:05:07 -0300

    Feat / Style
    feat: Adiciona e atualiza imagens e logos da empresa
    
    - Substitui e adiciona novas imagens e logos da empresa.
    
    fix: Corrige altura irregular dos produtos
    
    - Ajusta a altura dos produtos para garantir consistência na exibição.
    
    feat: Introduz duas sugestões de layout de produto
    
    - Adiciona duas opções de layout de produto para escolha.
    
    feat: Desenvolve parcialmente o footer do site
    
    - Implementa uma parte do footer do site e completa 3 dos menus suspensos.
    
    feat: Adiciona localização ao site
    
    - Inclui informações de localização no site.
    
    refactor: Melhora partes do código e remove código desnecessário
    
    - Aprimora o código existente, eliminando elementos inúteis.
    
    style: Ajusta detalhes de estilo
    
    - Faz ajustes em bordas e outros detalhes de estilo.

--- Js/js.js
+++ Js/js.js
@@ -1,4 +1,13 @@
-document.getElementById('menu-button').addEventListener('click', function () {
-    document.getElementById('menu').classList.toggle('active');
-    document.querySelector('.content').classList.toggle('active');
+const menuButtons = document.querySelectorAll('.menu-button');
+
+// Adicione um evento de clique a cada botão
+menuButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.getAttribute('data-target');
+        const menu = document.querySelector(`.${target}`);
+        const content = document.querySelector(`.content-${target}`);
+
+        menu.classList.toggle('active');
+        content.classList.toggle('active');
+    });
 });
