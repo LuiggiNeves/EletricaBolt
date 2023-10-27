39f3eeb2b208be6774c5102a92e948f8f474d131
Luiggi Neves <luiggineves21@gmail.com>
2023-10-26 21:49:46 -0300

    feat
    feat: Adiciona funcionalidade de rotação automática ao slide
    
    - Implementa uma função para fazer com que as páginas do slide mudem automaticamente após um intervalo de tempo.
    - Adiciona dois botões manuais que permitem ao usuário reiniciar o tempo automático para uma melhor experiência.
    - Introduz novos temas visuais para o slide, melhorando a estética e a personalização.
    - Inclui imagens de produtos em formato PNG para aprimorar a apresentação dos produtos no slide.
    - Corrige a transparência da logo do site para garantir uma aparência mais limpa.
    - Integra um mapa próximo ao rodapé para fornecer informações de localização.
    
    # ----

--- Js/js.js
+++ Js/js.js
@@ -11,3 +11,41 @@ menuButtons.forEach(button => {
         content.classList.toggle('active');
     });
 });
+
+
+
+let slideIndex = 1;
+let slideTimer; // Variável para armazenar o temporizador
+
+showSlides(slideIndex);
+
+// Função para avançar/retroceder manualmente
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  restartTimer(); // Reinicia o temporizador quando a navegação manual é acionada
+}
+
+// Função para exibir slides automaticamente
+function autoSlides() {
+  showSlides(slideIndex += 1);
+  slideTimer = setTimeout(autoSlides, 10000); // Muda o slide a cada 5 segundos
+}
+
+// Função para mostrar slides
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Função para reiniciar o temporizador
+function restartTimer() {
+  clearTimeout(slideTimer);
+  slideTimer = setTimeout(autoSlides, 5000);
+}
+
+autoSlides(); // Inicia o efeito automático
