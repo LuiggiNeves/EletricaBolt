13d94a9df77e8212da55881b8ae4843f91c3a87f
LuiggiNeves <luiggineves21@gmail.com>
2024-01-10 01:36:02 -0300

    Feat Preview produtos
    Falta responsividade e style
    
    adicionado o preview-products em um doc .php
    
    Foram separados os menus nav e footer
    
    Removido o Model preview produtos

---
+++ app/view/pages/navbar.php
@@ -0,0 +1,119 @@
+<header>
+        <div class="header-primary">
+            <div class="header-container header-primary-color header-container-primary">
+                <div class="social-ico-container">
+                    <div class="social-ico-img ">
+                        <div class="ico-one ico-generic">
+                            <a href=""><img src="<?php echo HOST_APP; ?>/app/view/images/wpp-ico.png" alt=""></a>
+                        </div>
+                        <div class="ico-two ico-generic">
+                            <a href="https://www.instagram.com/eletricabolt/" target="_blank"><img src="<?php echo HOST_APP; ?>/app/view/images/ig-ico.png" alt=""></a>
+                        </div>
+
+                        <div class="ico-tree ico-generic">
+                            <a href="https://www.facebook.com/eletricaboltLim" target="_blank"><img src="<?php echo HOST_APP; ?>/app/view/images/fc-ico.png" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="social-name-center social-generic">
+                        <p>Bem vindo ao site da <strong class="light-text">Elétrica Bolt!</strong></p>
+                    </div>
+
+                    <div class="social-name-partnes social-generic">
+                        <a href="https://www.bing.com/ck/a?!&&p=3055a31e23cdc5c3JmltdHM9MTY5ODEwNTYwMCZpZ3VpZD0yNDJlYmY1ZS04NDc3LTZmYzktMzBhOS1hYzQ1ODUwZTZlMmYmaW5zaWQ9NTgzOA&ptn=3&hsh=3&fclid=242ebf5e-8477-6fc9-30a9-ac45850e6e2f&u=a1L21hcHM_Jm1lcGk9MTM0fn5Vbmtub3dufkFkZHJlc3NfTGluayZ0eT0xOCZxPUVsZXRyaWNhJTIwQm9sdCZzcz15cGlkLllONzk5M3g4NDcxNjUwMzkxMTM3NDEyMzE2JnBwb2lzPS0yMi41OTYwNDgzNTUxMDI1NF8tNDcuNDIxNDA1NzkyMjM2MzNfRWxldHJpY2ElMjBCb2x0X1lONzk5M3g4NDcxNjUwMzkxMTM3NDEyMzE2fiZjcD0tMjIuNTk2MDQ4fi00Ny40MjE0MDYmdj0yJnNWPTE&ntb=1" target="_blank">Localização</a>
+                        <a href="https://www.instagram.com/eletricabolt/" target="_blank">Parceiros</a>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="hr-headr">
+
+            <div class="header-container">
+                <div class="menu-search-container">
+                    <div class="menu-search">
+                        <div class="container-menu-logo">
+                            <div class="menu-logo">
+                                <div class="logo">
+                                    <div class="img-logo">
+                                        <a href=""><img src="<?php echo HOST_APP; ?>/app/view/images/bolt-logo-completa-am.png" alt=""></a>
+                                    </div>
+                                    <div class="text-logo">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-container-princ">
+                            <div class="search-container search-container-paralel">
+                                <div class="search-bar-header">
+                                    <input type="text" class="search-bar" placeholder="Pesquisar...">
+                                </div>
+                                <div class="search-container-btn">
+                                    <button class="search-button" id="search-button">
+                                        <p>Buscar</p>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ico-user">
+                            <div class="ico-user-container">
+                                <div class="ico-user-one ico-user-generic">
+                                    <a href="html/login.html"><img src="<?php echo HOST_APP; ?>/app/view/images/person-ico.png" alt=""></a>
+                                </div>
+                                <div class="ico-user-two ico-user-generic">
+                                    <a href="#" onclick="openModal()"><img src="<?php echo HOST_APP; ?>/app/view/images/bag-ico.png" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="header-container container-header-two">
+                <div class="nav-container">
+                    <div class="nav-box dropdown_div nav_position">
+                        <div class="nav-item-one nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                                
+                            </div>
+                        </div>
+
+                        <div class="nav-item-two nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                                
+                            </div>
+                        </div>
+
+                        <div class="nav-item-tree nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                            </div>
+                        </div>
+
+                        <div class="nav-item-four nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                            </div>
+                        </div>
+
+                        <div class="nav-item-five nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                            </div>
+                        </div>
+
+                        <div class="nav-item-six nav-item-generic dropdown">
+                            <p>Inserir aqui</p>
+                            <div class="dropdown-content">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
