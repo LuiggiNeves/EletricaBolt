d618f72327a02fd003fcecca934d1a0bcf92530c
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-01-13 15:42:58 -0300

    feat: página pública para visualizar produto selecionado

---
+++ app/view/js/inicio_produtos.js
@@ -0,0 +1,69 @@
+function leDadosDoProdutoPorId(id) {
+    let formData = new FormData();
+    formData.append("route", "le-produto-por-id");
+    formData.append("id", id);
+
+    post("../../app/controller/http/controller.php", formData,
+        function (response) {
+            let dados = response["dados"];
+            let mensagem = response["mensagem"];
+
+            let produto = dados["produto"];
+
+            carregaViewDoProduto(
+                produto["nome"],
+                produto["preco"],
+                produto["categoria"],
+                produto["codigo_referencia"],
+                produto["descricao"]
+            );
+
+            carregaImagensDoProduto(
+                produto["imagens"]
+            );
+        },
+        "",
+        function () {
+
+        }
+    );
+}
+
+function carregaViewDoProduto(nome, preco, categoria, codigo_de_referencia, descricao) {
+    $("#nomeDoProduto").text(nome);
+    $("#precoDoProduto").text(preco);
+
+    if (categoria == null) {
+        $("#divCategoriaDoProduto").hide();
+    } else {
+        $("#divCategoriaDoProduto").text(categoria);
+    }
+
+    $("#codigoDeReferenciaDoProduto").text(codigo_de_referencia);
+    $("#descricaoDoProduto").text(descricao);
+}
+
+function carregaImagensDoProduto(imagens) {
+    let imagem_padrao = `../../app/view/images/produto.png`;
+    if (Array.isArray(imagens)) {
+        for (let i = 0; i < imagens.length; i++) {
+            let path_imagem = imagens[i]["imagem_path"] != null ? `../../app/files/entities/` + imagens[i]["imagem_path"] + `` : imagem_padrao;
+            if (i == 0) {
+                imagem_padrao = path_imagem;
+            }
+
+            $(".multiple-imgs").append(
+                `
+                    <img src="`+ path_imagem + `" class="w-100" />
+                `
+            );
+        }
+    }
+
+    $("#imagemDoProduto").attr("src", imagem_padrao);
+}
+
+
+$(document).ready(function () {
+
+});
